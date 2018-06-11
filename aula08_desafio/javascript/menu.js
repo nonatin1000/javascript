af6a2a36d07164ce52fd10e5a4a63a43c1cbaf68
Nonato Sales <nrdesales@hotmail.com>
2018-06-11 11:26:06 -0300

    melhorias

--- aula08_desafio/javascript/menu.js
+++ aula08_desafio/javascript/menu.js
@@ -2,6 +2,9 @@
 	var html = document.querySelector('html');
 	var btn = document.querySelector('.header-nav__hamburgger');
 
+	html.classList.remove('no-js');
+	html.classList.add('js');
+
 	btn.addEventListener('click', function (){
 		html.classList.toggle('menu-opened');
 	});
